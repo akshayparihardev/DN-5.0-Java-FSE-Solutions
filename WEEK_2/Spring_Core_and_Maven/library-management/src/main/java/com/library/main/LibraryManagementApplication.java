5d0ed54484ba5c9e03b917092f89c8a8117d3b0c
Akshay Parihar <akshayparihar9310@gmail.com>
2026-07-06 19:59:15 +0530

    Completed Exercise 2: Implementing Dependency Injection

--- WEEK_2/Spring_Core_and_Maven/library-management/src/main/java/com/library/main/LibraryManagementApplication.java
+++ WEEK_2/Spring_Core_and_Maven/library-management/src/main/java/com/library/main/LibraryManagementApplication.java
@@ -3,7 +3,6 @@ package com.library.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.library.repository.BookRepository;
 import com.library.service.BookService;
 
 public class LibraryManagementApplication {
@@ -13,13 +12,9 @@ public class LibraryManagementApplication {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        BookRepository repository =
-                context.getBean("bookRepository", BookRepository.class);
-
         BookService service =
                 context.getBean("bookService", BookService.class);
 
-        repository.display();
         service.display();
 
         ((ClassPathXmlApplicationContext) context).close();
