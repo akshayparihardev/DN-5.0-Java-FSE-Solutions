453f1be2c1f646bb43decb2dc97f32c1e6a6fade
Akshay Parihar <akshayparihar9310@gmail.com>
2026-07-15 11:09:29 +0530

    Complete Handson 6 : React Router

---
+++ WEEK_5/React/Handson_6/trainersapp/src/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Trainers Application</h2>
+      <p>Select Trainers from the menu.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ WEEK_5/React/Handson_6/trainersapp/src/TrainersMock.js
@@ -0,0 +1,30 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+  new Trainer(
+    1,
+    "John Doe",
+    "john@gmail.com",
+    "9876543210",
+    "Java",
+    "Spring Boot, Hibernate"
+  ),
+  new Trainer(
+    2,
+    "Jane Smith",
+    "jane@gmail.com",
+    "9876501234",
+    "React",
+    "React, JavaScript, CSS"
+  ),
+  new Trainer(
+    3,
+    "Robert Brown",
+    "robert@gmail.com",
+    "9123456789",
+    ".NET",
+    "ASP.NET, C#"
+  )
+];
+
+export default trainers;
