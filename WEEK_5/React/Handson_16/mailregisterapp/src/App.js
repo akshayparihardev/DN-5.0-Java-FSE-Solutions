950c9705fffd15fe9e0ffea516e36dc9573350cf
Akshay Parihar <akshayparihar9310@gmail.com>
2026-07-21 23:11:36 +0530

    Complete Handson 16 : Form Validation

---
+++ WEEK_5/React/Handson_16/mailregisterapp/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+import Register from "./Register";
+
+function App() {
+  return <Register />;
+}
+
+export default App;
